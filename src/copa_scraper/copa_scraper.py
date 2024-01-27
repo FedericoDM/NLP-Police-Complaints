80277f5790d13fe0dae69822b84a2cfa3ed8705c
Federico Domínguez Molina <fd.molina@outlook.com>
2024-01-26 21:41:03 -0600

    Succesfully extracted first table

--- src/copa_scraper/copa_scraper.py
+++ src/copa_scraper/copa_scraper.py
@@ -4,10 +4,34 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# URL of the COPA website
+
+# PARAMETERS
 URL = "https://www.chicagocopa.org/data-cases/case-portal/"
+headers = {
+    "Referer": "https://www.chicagocopa.org/data-cases/case-portal/",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+}
 
 
 class COPAScraper:
+    URL = URL
+
     def __init__(self):
-        pass
+        self.headers = headers
+
+    def extract_table(self, url):
+        """Extracts the table from the given url"""
+        # Use pandas
+        table_index = 0
+        r = requests.get(url, headers=self.headers)
+        soup = BeautifulSoup(r.content, "html.parser")
+        table = soup.find("table")
+        df = pd.read_html(str(table))[table_index]
+        return df
+
+
+if __name__ == "__main__":
+    scraper = COPAScraper()
+    tables = scraper.extract_table(URL)
+    print(tables)
