5ba696f8a63002aabc80c698e31e705e74561ecf
Federico Domínguez Molina <fd.molina@outlook.com>
2024-01-26 22:47:26 -0600

    Adding URL to pdf in Dataframe

--- src/copa_scraper/copa_scraper.py
+++ src/copa_scraper/copa_scraper.py
@@ -21,6 +21,29 @@ class COPAScraper:
         self.headers = headers
         self.case_url = "https://www.chicagocopa.org/case"
 
+    @staticmethod
+    def construct_pdf_url(df):
+        """Constructs the pdf url given the case number and
+        the FSR Posted Date if available"""
+
+        # Extract case number
+        case_number = df["Log#"]
+
+        # We need month and year of FSR Posted Date
+        raw_date = str(df["FSR/Memo Posted Date"])
+
+        if "/" in raw_date:
+            # Extract month and year
+            split_date = raw_date.split("/")
+            month = split_date[0]
+            year = split_date[2]
+
+            # Construct url
+            return f"https://www.chicagocopa.org/wp-content/uploads/{year}/{month}/{case_number}_FSR.pdf"
+
+        else:
+            return "Not Available"
+
     def extract_table(self, url):
         """Extracts the table from the given url"""
         # Use pandas
@@ -31,9 +54,10 @@ class COPAScraper:
         df = pd.read_html(str(table))[table_index]
 
         # Construct case URLs
-        df.loc[:, "case_url"] = df["Case Number"].apply(
-            lambda x: f"{self.case_url}/{x}/"
-        )
+        df.loc[:, "case_url"] = df["Log#"].apply(lambda x: f"{self.case_url}/{x}/")
+
+        # Construct PDF URLs
+        df.loc[:, "pdf_url"] = df.apply(self.construct_pdf_url, axis=1)
 
         return df
 
@@ -41,4 +65,5 @@ class COPAScraper:
 if __name__ == "__main__":
     scraper = COPAScraper()
     tables = scraper.extract_table(URL)
+    tables.to_csv("copa_data.csv", index=False)
     print(tables)
